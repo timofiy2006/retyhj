1d0722730848221b76e9892c7e60cd04ea7b771a
timofiy2006 <146582193+timofiy2006@users.noreply.github.com>
2024-02-19 21:50:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#select = input('введите свой инградиент')
+#select2 = input('введите свой инградиент')
+shawuha = ['meat', 'lavash','souse']
+pizza = ['cheese', 'souse', 'bread']
+borsh = ['burak', 'meat', 'potato' ]
+#print(shawuha, pizza, borsh)
+shawufa_opis="самая крутая шавуха в мире"
+pizza_opis="самая крутая пица в мире"
+borsh_opis="самый крутой борщ в мире"
+menu = {'meat': 3,
+        'cheese': 69,
+        'souse': 6,
+        'potato': 5,
+        'burak': 14
+         }
+print (menu)
+print (type(shawuha), type(pizza), type(borsh))
+print (shawufa_opis, pizza_opis,borsh_opis )
